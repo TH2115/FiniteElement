6c68a16822493cb37fc8b88135936ddd61e15de7
Trieu Ho <th2115@imperial.ac.uk>
2018-03-19 13:51:42 +0000

    has mesh generator

---
+++ meshGen.cpp
@@ -0,0 +1,17 @@
+#include <meshGen.h>
+
+
+void createLinspace(double* Vect, double mini, double maxi, int n){
+    double delta = (maxi - mini)/double(n-1);
+    for(int i = 0; i < n; i++){
+        Vect[i] = mini + double(i)*delta;
+        }
+}
+
+
+void heightFunction(int n, double* x, double a, double b, double h1, double* h){
+    for (int i =0; i < n; i++){
+        h[i] = (a * x[i] * x[i] + b * x[i] + h1);
+    }
+}
+
---
+++ meshGen.h
@@ -0,0 +1,11 @@
+#ifndef MESHGEN_H_INCLUDED
+#define MESHGEN_H_INCLUDED
+
+void createLinspace(double* Vect, double mini, double maxi, int n);
+
+void heightFunction(int n, double* x, double a, double b, double h1, double* h);
+
+
+
+
+#endif // MESHGEN_H_INCLUDED
